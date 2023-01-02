0b4b68f5681ae6416e73086eaefdc9e20c7d643d
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-02 22:59:24 +0200

    wildcard Route added

--- app/app.js
+++ app/app.js
@@ -11,6 +11,12 @@ app.use(express.json())
 const userRoutes = require("../routes/user.routes")
 app.use("/api/user/",  userRoutes)
 
-
+app.all("*", (req, res)=> {
+    res.status(404).send({
+        apisStatus:false,
+        message:"Invalid URL",
+        data: {}
+    })
+})
 
 module.exports = app
