9c95ea11c557e0dc5cebc72a4ee965e83ea21e48
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-02 20:15:11 +0200

    Database connection module implemented

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const connectDB = require('../database/connection')
 
 app.use(cors())
+connectDB();
 app.use(express.json())
 
 
 
-module.exports=app
+module.exports = app
