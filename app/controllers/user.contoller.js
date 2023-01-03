f1334b74f67a084ff33e7c6a720496cb06da3163
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-03 14:47:37 +0200

    Signup method implemented

--- app/controllers/user.contoller.js
+++ app/controllers/user.contoller.js
@@ -1,8 +1,19 @@
 
 const userModel = require("../../database/models/user.model")
+const resHelper = require("../helpers/resHelper")
 
-
-class User { }
+class User {
+    static signUp = async (req, res) => {
+        try {
+            const userData = new userModel(req.body)
+            await userData.save()
+            myHelper.resHandler(res, 200, true, userData, "user added successfully")
+        }
+        catch (e) {
+            myHelper.resHandler(res, 500, false, e, e.message)
+        }
+    }
+}
 
 
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,9 +3,7 @@ const router = require("express").Router()
 const User = require("../app/controllers/user.contoller")
 
 // Sign Up account
-router.post("/signup", (req, res) => {
-    res.send("Singup Route")
-})
+router.post("/signup", User.signUp)
 
 // Login
 router.post("/login", (req, res) => {
