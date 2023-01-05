753bf6f1f1daa51558ec9048b2909940f11ac510
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-05 04:17:30 +0200

    Partner model implemented

---
+++ database/models/partner.model.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+
+const partnerSchema = Schema({
+    approved: {
+        type: Boolean,
+        default: false
+    },
+    storeName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    numberOfBranches: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    storeAddresses: [
+        {
+            email: {
+                type: String,
+                unique: true,
+                required: true,
+                trim: true
+            },
+            phoneNumber: {
+                type: Number,
+                unique: true,
+                required: true,
+                trim: true
+            },
+            webSite: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            bracnchNumber: {
+                type: Number,
+                unique: true,
+                required: true,
+                trim: true
+            },
+            branchLocation: {
+                type: String,
+                unique: true,
+                required: true,
+                trim: true
+            },
+            contactRole: {
+                type: String,
+                enum: ["Owner", "Co-owner", "Manager", "Employee"],
+                required: true
+            }
+
+        }
+    ]
+})
+
+
+
+
+const Partner = mongoose.model("Partner", partnerSchema);
+
+module.exports = Partner;
