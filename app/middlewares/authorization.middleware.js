e57355cac5b232cd4e312aa5652f66e5f2239ffb
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-05 04:59:16 +0200

    authorization middleware issue fiexed

--- app/middlewares/authorization.middleware.js
+++ app/middlewares/authorization.middleware.js
@@ -1,22 +1,28 @@
 const routesModel = require("../../database/models/routes.model")
+const { route } = require("../app")
 const resHelper = require("../helpers/resHelper")
 
 const authorization = async (req, res, next) => {
     const currentUrl = req.originalUrl
     try {
         const routes = await routesModel.findOne({ url: currentUrl })
-        const findRule = routes.roles.find(role => {
-            return role.toString() == req.user.role.toString()
-        })
-        if (findRule.toString()) {
-            next()
+        if (!routes) {
+            resHelper.resHandler(res, 404, false, routes, "Route is not exist into routes data colection")
         } else {
-            resHelper.resHandler(res, 401, false, e.message, "Unauthorized request")
+            const findRule = routes.roles.find(role => {
+                return role.toString() == req.user.role.toString()
+            })
+            if (findRule.toString()) {
+                next()
+            } else {
+                resHelper.resHandler(res, 401, false, null, "Unauthorized request")
 
+            }
         }
+
     }
-    catch (e) {
-        resHelper.resHandler(res, 500, false, e.message, "Unauthorized request")
+    catch (err) {
+        resHelper.resHandler(res, 500, false, err.message, "Unauthorized request")
     }
 }
 module.exports = { authorization }
